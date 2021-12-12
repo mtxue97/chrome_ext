bfc7caf7c916c85bf32f1f4a2c72f52902f67944
mtxue97 <424307640@qq.com>
2021-12-12 22:36:59 +0800

    add

--- js/popup.js
+++ js/popup.js
@@ -2,25 +2,35 @@ var bg = chrome.extension.getBackgroundPage();
 bg.test(); // 访问backgrouund中的函数
 // alert(bg.document.body.innerHTML); // 访问bg的DOM
 
-// 向content-script注入JS片段
-function executeScriptToCurrentTab(code)
-{
-	getCurrentTabId((tabId) =>
-	{
-		chrome.tabs.executeScript(tabId, {code: code});
-	});
+// 向content-script注入代码片段
+function executeScriptToCurrentTab(code) {
+  getCurrentTabId((tabId) => {
+    chrome.tabs.executeScript(tabId, { code: code });
+  });
 }
 
 // 获取当前选项卡ID
-function getCurrentTabId(callback)
-{
-	chrome.tabs.query({active: true, currentWindow: true}, function(tabs)
-	{
-		if(callback) callback(tabs.length ? tabs[0].id: null);
-	});
+function getCurrentTabId(callback) {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    if (callback) callback(tabs.length ? tabs[0].id : null);
+  });
 }
 
 // 修改背景色
-$('#update_bg_color').click(() => {
-	executeScriptToCurrentTab('document.body.style.backgroundColor="red";')
+$("#update_bg_color").click(() => {
+  const code = 'document.body.style.backgroundColor="red";';
+  executeScriptToCurrentTab(code);
 });
+
+/**
+ *页面加滤镜
+ *
+ */
+
+$("#change_bg").click(() => {
+  changeBackgroundStyle();
+});
+function changeBackgroundStyle() {
+  const code = 'document.body.style.filter = "grayscale(3)";';
+  executeScriptToCurrentTab(code);
+}
